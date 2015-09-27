16cf27dfde6ad0498f514d18a1a869f248700505
Angel Gao <angel_gao@live.ca>
2015-09-27 13:32:20 -0400

    writing to prediction file

--- genderBasedModel.py
+++ genderBasedModel.py
@@ -30,3 +30,27 @@ proportion_men_survive = np.sum(men_onboard) / np.size(men_onboard)
 
 print "women who survive: ", proportion_women_survive
 print "men who survive: ", proportion_men_survive
+
+"""
+Writing to the Prediction file
+"""
+
+#reading from CSV file
+test_file = open('./csv/test.csv', 'rb')
+test_file_object = csv.reader(test_file)
+
+# Skip header of file
+header = test_file_object.next()
+
+prediction_file = open("./csv/genderbasedmodel.csv", "wb")
+prediction_file_object = csv.writer(prediction_file)
+
+prediction_file_object.writerow(["PassengerId", "Survived"])
+for row in test_file_object:       # For each row in test.csv
+    if row[3] == 'female':         # is it a female, if yes then                                       
+        prediction_file_object.writerow([row[0],'1'])    # predict 1
+    else:                              # or else if male,       
+        prediction_file_object.writerow([row[0],'0'])    # predict 0
+
+test_file.close()
+prediction_file.close()
